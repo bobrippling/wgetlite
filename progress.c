dc65ff3694534a28e85600c4385f658137658bed
Rob Pilling <rob pilling at gmail>
2011-03-21 15:52:20 +0000

    Minor changes for finishing

--- progress.c
+++ progress.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "progress.h"
 
-#define PROGRESS_RESET "\r\x1b[K"
+#define CLR_TO_EOL "\x1b[K"
 
 void progress_unknown()
 {
@@ -9,20 +9,24 @@ void progress_unknown()
 	printf("\r%c...", "/-\\|"[i++ % 4]);
 }
 
-void progress_fin()
+void progress_fin(int this, int total)
 {
+	if(this)
+		progress_show(this, total);
+	else
+		progress_unknown();
 	putchar('\n');
 }
 
 void progress_incomplete()
 {
-	puts(PROGRESS_RESET "Unexpected end-of-stream");
+	puts("\r" CLR_TO_EOL "Unexpected end-of-stream");
 }
 
 void progress_show(int this, int total)
 {
 #define PROG(fmt, div)  \
-		printf(PROGRESS_RESET "%d%% (%d " fmt " / %d " fmt ")", percent, this / div, total / div)
+		printf(CLR_TO_EOL "%d%% (%d " fmt " / %d " fmt ")\r", percent, this / div, total / div)
 
 	register int percent = 100.0f * (float)this / (float)total;
 
--- progress.h
+++ progress.h
@@ -2,7 +2,7 @@
 #define PROGRESS_H
 
 void progress_show(int this, int total);
-void progress_fin(void);
+void progress_fin( int this, int total);
 void progress_incomplete(void);
 void progress_unknown(void);
 
