6b3faf5ae3a2d1a2aeb9b12ed7f2c5c52e9e11b0
Rob <rob@jeffraw>
2011-03-29 17:42:14 +0100

    Fixed "dot"-progress

--- progress.c
+++ progress.c
@@ -26,56 +26,54 @@ void progress_fin(int this, int total)
 		else if(PROGRESS_TERMINAL())
 			progress_unknown();
 
-		fputc('\n', stderr);
+		if(PROGRESS_TERMINAL())
+			fputc('\n', stderr);
 	}
 }
 
 void progress_incomplete()
 {
-	if(PROGRESS_CAN_SHOW()){
 #define EOS "Unexpected end-of-stream"
+
+	if(PROGRESS_CAN_SHOW()){
 		if(PROGRESS_TERMINAL())
 			fputs("\r" CLR_TO_EOL EOS "\n", stderr);
 		else
-			fputs("\n" EOS "\n", stderr);
-#undef EOS
+			fputs(EOS "\n", stderr);
 	}
+
+#undef EOS
 }
 
 void progress_show(int this, int total)
 {
-#define PROG(fmt, div)  \
-		fprintf(stderr, CLR_TO_EOL "%d%% (%d " fmt " / %d " fmt ")\r", percent, this / div, total / div)
-
 	if(PROGRESS_CAN_SHOW()){
 		register int percent = 100.0f * (float)this / (float)total;
+		char *pre, *post;
+		char *sizstr;
+		int div;
+
+		if(total > 1024){
+			div = 1024;
+			sizstr = "KB";
+		}else{
+			div = 1;
+			sizstr = "B";
+		}
 
 		if(PROGRESS_TERMINAL()){
-			if(this > 1024)
-				PROG("KB", 1024);
-			else
-				PROG("B", 1);
+			pre = CLR_TO_EOL;
+			post = "\r";
 		}else{
-			static int midblock = 0;
-			int x, y;
-			term_xy(&x, &y);
-
-			if(!midblock){
-				fprintf(stderr, " %04dB", this - this % 50); /* TODO: kb */
-				midblock = 1;
-			}
-
-			while(this > 0){
-				int blockno = this - this % 10;
-				int dots = this - 10;
-
-				while(dots-- > 0)
-					fputc('.', stderr);
-
-				this -= 10;
-
-				fputc(' ', stderr);
-			}
+			pre = "";
+			post = "\n";
 		}
+
+		fprintf(stderr,
+				"%s%d%% (%d %s / %d %s)%s",
+				pre, percent,
+				this  / div, sizstr,
+				total / div, sizstr,
+				post);
 	}
 }
