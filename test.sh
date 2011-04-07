30904ca8a211e706150114d586303918c0c559bb
Rob Pilling <rob pilling at gmail>
2011-04-07 19:39:42 +0100

    New test code

--- test.sh
+++ test.sh
@@ -1,37 +1,69 @@
 #!/bin/sh
 
-etest(){
-	printf '\e[1;34m%s\e[1;0m\n' "$@"
-}
-
 WL=./wgetlite
-
 set -e
 
-# general tests
+etest(){
+	printf '\e[1;34m%s\e[m\n' "$@"
+}
 
-etest 1
-$WL -O /dev/null http://www.google.com/
+usage(){
+	echo "Usage: $0 [http|ftp]" >&2
+	exit 1
+}
 
-etest 2
-$WL -O /dev/null http://www.google.com:80/
+http(){
+	etest 1
+	$WL -vvv -O /dev/null http://www.google.com/
 
-etest 3
-$WL -O - http://www.google.com:80/
+	etest 2
+	$WL -U '' -O- http://www.google.com:80/ > /dev/null
 
-etest 4
-$WL http://www.google.com/
+	etest 3
+	$WL -c -O/dev/stderr -o /dev/stdout http://www.google.com:80/ 2> /dev/null
 
-etest 5
-$WL http://www.google.com:80/
+	etest 4
+	$WL -O/dev/null google.co.uk
 
-etest 6
-$WL www.google.com/
+	etest 5
+	$WL -O- www.google.com:80 > /dev/null
+}
 
-etest 7
-$WL www.google.com:80/
+ftp(){
+	# only need to test the protocol, rest is url parsing stuff
 
-# only need to test the protocol, rest is parsing stuff
+	f=/tmp/wgetlite_test
 
-etest 8
-$WL ftp://ftp.gnu.org/find.txt.gz
+	trap "rm -f $f" EXIT
+
+	etest 6
+	$WL -O $f ftp://ftp.gnu.org/find.txt.gz
+
+	len=$(expr $(stat -c '%s' $f) / 2)
+
+	truncate -s$len $f
+
+	etest 7
+	$WL -c -O $f ftp://ftp.gnu.org/find.txt.gz
+}
+
+if [ $# -eq 1 ]
+then
+	case "$1" in
+		ftp)
+			ftp
+			;;
+		http)
+			http
+			;;
+		*)
+			usage
+			;;
+	esac
+	exit
+elif [ $# -ne 0 ]
+then usage
+fi
+
+http
+ftp
